04f6cd5be32252d6af8ded9b66555b7ed8dc2940
Alex Smekhnov <alexander.smehnov@gmail.com>
2024-02-08 23:10:36 +0300

    change playing duration and release camera stream

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # Play audiosample
 import os
 audio_filename = "file_example_MP3_2MG.mp3"
-os.system(f"ffplay -nodisp -autoexit -loglevel quiet {audio_filename}")
+os.system(f"ffplay -nodisp -t 6 -autoexit -loglevel quiet {audio_filename}")
 
 # Capture image
 import cv2
 camera_capture = cv2.VideoCapture(0)
 rv, image = camera_capture.read()
 print(f"Image Dimensions: {image.shape}")
+camera_capture.release()
 
 # Capture audio sample
 print("Start recording audio")
